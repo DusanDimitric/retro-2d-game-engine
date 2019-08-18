3d50dce83c946a52b37a1fc3de18cc6330ac54a9
Dusan Dimitric <dusan@mitipi.com>
2019-08-18 11:35:26 +0200

    Update the Grid

--- src/app/domain/Grid.ts
+++ src/app/domain/Grid.ts
@@ -1,6 +1,18 @@
 import * as CONFIG from '@app/configuration/config.json'
 
+// TODO: This class is useless?
 export default class Grid {
-  rows: number = CONFIG.CANVAS_HEIGHT / CONFIG.TILE_SIZE
-  cols: number = CONFIG.CANVAS_WIDTH  / CONFIG.TILE_SIZE
+  public rows: number = CONFIG.CANVAS_HEIGHT / CONFIG.TILE_SIZE
+  public cols: number = CONFIG.CANVAS_WIDTH  / CONFIG.TILE_SIZE
+}
+
+export interface Directions {
+    N  : (any | null)
+    NE : (any | null)
+    E  : (any | null)
+    SE : (any | null)
+    S  : (any | null)
+    SW : (any | null)
+    W  : (any | null)
+    NW : (any | null)
 }
